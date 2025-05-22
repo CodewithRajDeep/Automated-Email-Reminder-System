493aabcf2e3ea3d2330b897077c286af1d4293e2
Deep Raj <deepraj6897@gmail.com>
2025-05-22 09:05:14 +0530

    added string connection

--- app.js
+++ app.js
@@ -8,5 +8,22 @@ const nodemailer = require('nodemailer');
 const expresseLayouts = require('express-ejs-layouts');
 const reminder = require('./models/reminder');
 
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.json());
+
+//ejs-template engine
+app.set('view engine', 'ejs');
 
 
+app.set('views', path.join(__dirname, 'views'));
+app.set(expresseLayouts);
+app.set('layout', "layout");
+
+mongoose.connect(process.env.MONGO_DB_URL).then(() => {
+    console.log('connected to database');
+}).catch((err) => {
+    console.log(`Error: conncting to mongodb: ${err.message};`);
+});
