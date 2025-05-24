39d39d363fd92c8cd7082043f2b99cf37b0fba19
Deep Raj <deepraj6897@gmail.com>
2025-05-24 20:28:29 +0530

    added relative style

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get('/', (req,res) => {
 
 app.get('/about', (req, res) => {
   res.render('about', {
-    title: 'About Email Reminder App',
+    title: 'About - Email Reminder App',
     currentPage: 'about'
   });
 });
