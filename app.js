5d716922b2c773ff06d1c537da0c0b482eee8ecd
Deep Raj <deepraj6897@gmail.com>
2025-05-24 21:45:04 +0530

    added route for schedule

--- app.js
+++ app.js
@@ -45,5 +45,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/schedule', (req, res) => {
+  res.render('schedule', {
+    title: 'Schedule Reminder',
+    currentPage: 'about'
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
